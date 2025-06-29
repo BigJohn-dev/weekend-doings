3e47bbe49789f6f4f53c2c3224e4df8563cb84d0
John Imeobong <imeobongjohn38@gmail.com>
2025-06-30 00:37:24 +0100

    Add files via upload

--- Weekend doings/AirConditioner/AC/DSA/AirConditioner.java
+++ Weekend doings/AirConditioner/AC/DSA/AirConditioner.java
@@ -3,19 +3,22 @@ package DSA;
 public class AirConditioner {
 
     private boolean status;
-    private int temperature;
+    private int temperature = 16;
 
-    public void turnOn() {
-        this.status = true;
-    }
-    public void turnOff(){
-        this.status = false;
-    }
-    public boolean isOn(){
-        return this.status;
+    public void turnOn() { this.status = true;}
+    public void turnOff(){ this.status = false;}
+    public boolean isOn() {return this.status;}
+
+    public int getTemperature(){ return this.temperature;}
+
+    public void increaseTemperature(int increment) {
+        this.temperature += increment;
+        if (temperature >= 30) this.temperature = 30;
     }
 
-    public void increaseTemperature() {
-        temperature++;
+    public void decreaseTemperature(int decrement) {
+        this.temperature -= decrement;
+        if (temperature <= 16) this.temperature = 16;
     }
+
 }
--- Weekend doings/AirConditioner/test/DSA/AirConditionerTest.java
+++ Weekend doings/AirConditioner/test/DSA/AirConditionerTest.java
@@ -27,10 +27,31 @@ public class AirConditionerTest {
     }
 
     @Test
-    public void testAirConditionerTemperature() {
+    public void testAirConditionerTemperatureIncrease() {
         currentState.turnOn();
-        currentState.increaseTemperature();
-        assertEquals( , currentState.increaseTemperature());
+        currentState.increaseTemperature(20);
+        assertEquals(30, currentState.getTemperature());
     }
 
+    @Test
+    public void testAirConditionerTemperatureDecrease() {
+        currentState.turnOn();
+        currentState.increaseTemperature(4);
+        currentState.decreaseTemperature(10);
+        assertEquals(16, currentState.getTemperature());
+    }
+
+    @Test
+    public void testAirConditionerTemperatureDoesNotIncreaseBeyond_30Degrees() {
+        currentState.turnOn();
+        currentState.increaseTemperature(36);
+        assertEquals(30, currentState.getTemperature());
+    }
+
+    @Test
+    public void testAirConditionerTemperatureDoesNotDecreaseBelow_16Degrees() {
+        currentState.turnOn();
+        currentState.decreaseTemperature(10);
+        assertEquals(16, currentState.getTemperature());
+    }
 }
